158f4a419471c5242e7697476f9a7caf4f4a2360
Mehedee Hassan <razin.meha@gmail.com>
2017-11-20 15:22:37 +0600

    add act settings

---
+++ app/src/main/java/wechat/liquiddark/com/wechat/activity/SettingsActivity.java
@@ -0,0 +1,15 @@
+package wechat.liquiddark.com.wechat.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import wechat.liquiddark.com.wechat.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
